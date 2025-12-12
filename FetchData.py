1d8ec36e78feee448a70fd7cf66fb66c0924e7e8
Théo Maratrey <theo.maratrey@students.fhnw.ch>
2025-12-12 09:51:56 +0100

    Update FetchData.py

--- FetchData.py
+++ FetchData.py
@@ -2,15 +2,31 @@ import requests
 
 url = "https://api.opentransportdata.swiss/TDP/Soap_Datex2/Pull"
 
-# Defines the request Token and Hash between quotes
+soap_body = """<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+  <soapenv:Header/>
+  <soapenv:Body>
+    <d2LogicalModel xmlns="http://datex2.eu/schema/2/2_0"
+                     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                     modelBaseVersion="2">
+      <exchange>
+        <supplierIdentification>
+          <country>ch</country>
+          <nationalIdentifier>MYAPP</nationalIdentifier>
+        </supplierIdentification>
+      </exchange>
+    </d2LogicalModel>
+  </soapenv:Body>
+</soapenv:Envelope>
+"""
+
 headers = {
-    "Authorization": "eyJvcmciOiI2NDA2NTFhNTIyZmEwNTAwMDEyOWJiZTEiLCJpZCI6ImQ4ZjdhMGQ0NTg0ZjRmMzliODI4YTNmZDdjNjdiMWI4IiwiaCI6Im11cm11cjEyOCJ9",
-    "X-Hash": "60e8abbe9205f993a413708970bad626"
+    "Authorization": f"Bearer eyJvcmciOiI2NDA2NTFhNTIyZmEwNTAwMDEyOWJiZTEiLCJpZCI6ImQ4ZjdhMGQ0NTg0ZjRmMzliODI4YTNmZDdjNjdiMWI4IiwiaCI6Im11cm11cjEyOCJ9",
+    "Content-Type": "text/xml; charset=utf-8",
+    "SoapAction": "http://opentransportdata.swiss/TDP/Soap_Datex2/Pull/v1/pullMeasuredData"
 }
 
-# Sends the request and saves feedback in "response"
-response = requests.get(url, headers=headers)
+response = requests.post(url, data=soap_body, headers=headers)
 
-# Prints the feedback
-print(f"Status Code: {response.status_code}")
-print(f"Response: {response.text}")
+print("Status:", response.status_code)
+print(response.text[:500]) 
